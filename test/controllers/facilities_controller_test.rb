1fd3b6a96bedaa4ec83da3d29484d6bfe7d374ab
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-05-11 22:27:07 +0900

    applyed bootstrap

---
+++ test/controllers/facilities_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class FacilitiesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get facilities_index_url
+    assert_response :success
+  end
+
+end
