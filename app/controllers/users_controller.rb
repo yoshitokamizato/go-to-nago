a42c79864cac4cf8a069947571a74b85c28bcefb
tsubasa <tonochi.tsu@gmail.com>
2020-05-18 08:37:43 +0900

    マイページ実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,4 @@
+class UsersController < ApplicationController
+  def mypage
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+ 
   devise_for :users
   root to:'facilities#index'
   get '/facilities/list',to:'facilities#show'
+  get 'users/mypage',to: 'users#mypage'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
