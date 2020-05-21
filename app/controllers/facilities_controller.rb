b358e7c4abea1b7ef38fb21af3d0cf6fc67d55f9
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-05-21 22:28:55 +0900

    added facility_lilst and gourmet_list

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -3,9 +3,11 @@ class FacilitiesController < ApplicationController
   def index
   end
 
-  def show
-    @facilities = Facility.includes(:facility_images)
-    @menu = Menu.all
+  def show_facility
+    @facilities = Facility.includes(:facility_images).where(type:"facilities")
+  end
+  def show_gourmet
+    @gourmets = Facility.includes(:facility_images).where(type:"gourmet")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'facilities#index'
-  get '/facilities/list',to:'facilities#show'
+  get '/facility/list',to:'facilities#show_facility'
+  get '/gourmet/list',to:'facilities#show_gourmet'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
