7b293beb012f62550a9352ef5eed4d8353341334
福岡和樹 <kazuki@mba.local>
2020-05-24 22:01:31 +0900

    確認ページのリンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/facility/list',to:'facilities#show_facility'
   get '/gourmet/list',to:'facilities#show_gourmet'
   get 'users/mypage',to: 'users#mypage'
+  get 'users/confirm', to: 'users#confirm'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
