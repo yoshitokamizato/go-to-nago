79c8109b89695f8b1b9dadf0d5854f5fa587eb3a
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-05-27 00:59:29 +0900

    add bookmark custom

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -3,6 +3,7 @@ class FacilitiesController < ApplicationController
   def index
     @facilities = Facility.includes(:facility_images).where(type:"facilities")
     @gourmets = Facility.includes(:facility_images).where(type:"gourmet")
+    # @facilities = Facility.all.includes(:user)
   end
 
   def show_facility
@@ -14,6 +15,11 @@ class FacilitiesController < ApplicationController
 
   def show_gourmet
     @gourmets = Facility.includes(:facility_images).where(type:"gourmet")
+  end
+
+  def bookmarks
+    # has many throughのりレーションを組んだことで　current_user.bookmark_boardsで呼
+    @facilities = current_user.bookmark_facilities.includes(:user).recent
   end
 
 
