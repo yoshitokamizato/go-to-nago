f8b1cca4aaac271cf684e60ea1599231b409c471
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-07-11 18:50:06 +0900

    プロフィールページの編集機能を実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,8 +38,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
+    #必須  更新（編集の反映）時にパスワード入力を省く
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  #任意  更新後のパスを指定
+  def after_update_path_for(resource)
+    user_path(@user.id)
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   get 'bookmarks/create'
   get 'bookmarks/destroy'
-  devise_for :users
   root to:'facilities#index'
   get '/facility/list',to:'facilities#show_facility'
   get '/bookmarks',to:"facilities#bookmark"
@@ -17,7 +16,16 @@ Rails.application.routes.draw do
     get :bookmarks, on: :collection
   end
 
-  get 'users/show',to: 'users#show'
-  get 'users/confirm', to: 'users#confirm'
+  Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
+  end
+  resource :user ,only: [:show] do
+    collection do
+    # get 'new'
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
