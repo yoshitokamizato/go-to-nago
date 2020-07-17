ca9342c0f862b5ddf9911768d881c4d796ebea0a
MinoruKotegawa <61322479+MinoruKotegawa@users.noreply.github.com>
2020-07-18 07:33:32 +0900

    Update application.rb
    不要な改行の削除

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module GoToNago
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
