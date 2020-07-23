db7ab27caba420718fed7916b7bb42de3e9e131c
nobuosan <gariben.elsha.a27scrv@gmail.com>
2020-07-23 22:11:06 +0900

    password編集ページを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
+  # CSRF(クロスサイトリクエストフォージェリ) 対策
+  protect_from_forgery
   # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birth_year, :sex, :prefecture, :image, :profile])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :birth_year, :sex, :prefecture, :image, :profile])
+    # アップデートのストロングパラメータを設定
+    devise_parameter_sanitizer.permit(:update_password, keys: [:password,:password_confirmation ,:current_password])
   end
 end
