bc368757733302d546a87b64fe105cb10dc2bd4c
great084 <great084@gmail.com>
2020-07-29 02:12:42 +0900

    リファクタ controllerのロジックをmodelへ移管

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,57 +1,27 @@
 class InquiriesController < ApplicationController
 
-  NEW_SUBJECT = "情報掲載依頼"
-  NEW_MESSAGE = <<-TEXT 
-    情報掲載を希望される施設の情報を、記載してください。
-    ＝＝＝＝＝＝＝＝＝＝＝＝
-    施設名：
-    郵便番号、住所：
-    電話番号：
-    URL:
-    定休日：
-    営業時間：
-    メニュー：
-    駐車場：
-    TEXT
-  EDIT_SUBJECT = "情報修正依頼"
-  EDIT_MESSAGE = <<-TEXT 
-    情報掲載を希望される施設の情報を、記載してください。
-    ＝＝＝＝＝＝＝＝＝＝＝＝
-    修正が必要な項目：
-    正しい情報内容：
-    TEXT
-  
   def new
-    @inquiry = Inquiry.new # unless @inquiry
-    @inquiry.kind = params[:kind].to_i if params[:kind]
-    @inquiry.facility_id = params[:facility_id].to_i  if params[:facility_id]
-    debugger
-    case @inquiry.kind 
-    when "new_facility"
-      @inquiry.subject = NEW_SUBJECT
-      @inquiry.message = NEW_MESSAGE
-    when "edit_facility"
-      facility_name = @inquiry.facility.name
-      @inquiry.subject = EDIT_SUBJECT + "(#{facility_name})"
-      @inquiry.message = EDIT_MESSAGE
-    end
+    kind = params[:kind].present? ? params[:kind].to_i : nil
+    facility_id = params[:facility_id].present? ? params[:facility_id].to_i : nil
+    @inquiry = Inquiry.new
+    @inquiry.default_set(kind, facility_id)
+    # debugger
   end
 
   def confirm
     @inquiry = Inquiry.new(inquiry_params)
-    debugger
+    # debugger
     render :new if @inquiry.invalid?
   end
 
   def create
     @inquiry = Inquiry.new(inquiry_params)
-    debugger
+    # debugger
     # 戻るボタンが押された場合
     if params["back"]
       render :new
     elsif @inquiry.save
       flash.now[:success] = "お問い合わせを受け付けました"
-      # redirect_to root_url
       # メール送信
       # InquiryMailer.send_mail(@inquiry).deliver_later
     else
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -11,4 +11,39 @@ class Inquiry < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }
   validates :message, presence: true, length: { maximum: 1000 }
 
+  NEW_SUBJECT = "情報掲載依頼"
+  NEW_MESSAGE = <<-TEXT 
+    情報掲載を希望される施設の情報を、記載してください。
+    ＝＝＝＝＝＝＝＝＝＝＝＝
+    施設名：
+    郵便番号、住所：
+    電話番号：
+    URL:
+    定休日：
+    営業時間：
+    メニュー：
+    駐車場：
+    TEXT
+  EDIT_SUBJECT = "情報修正依頼"
+  EDIT_MESSAGE = <<-TEXT 
+    情報掲載を希望される施設の情報を、記載してください。
+    ＝＝＝＝＝＝＝＝＝＝＝＝
+    修正が必要な項目：
+    正しい情報内容：
+    TEXT
+
+  def default_set(kind, facility_id)
+    self.kind = kind.to_i
+    self.facility_id = facility_id
+    # debugger
+    case self.kind 
+    when "new_facility"
+      self.subject = NEW_SUBJECT
+      self.message = NEW_MESSAGE
+    when "edit_facility"
+      facility_name = Facility.find(self.facility_id).name
+      self.subject = EDIT_SUBJECT + "(#{facility_name})"
+      self.message = EDIT_MESSAGE
+    end
+  end
 end
