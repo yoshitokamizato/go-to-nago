6c0719bca161bbb34344cac4ba768fa50d3bcad1
MinoruKotegawa <kotegawa.1029@gmail.com>
2020-08-16 16:21:44 +0900

    add column mailmagazine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_133438) do
+ActiveRecord::Schema.define(version: 2020_08_16_054801) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "namespace"
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 2020_07_31_133438) do
     t.string "unconfirmed_email"
     t.string "provider"
     t.string "uid"
+    t.boolean "mailmagazine", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
