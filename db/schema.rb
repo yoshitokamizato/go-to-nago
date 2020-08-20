0c9fbad7f1435a4f7f5b397317a8693c2f2aa450
ひろみち <36526480+great084@users.noreply.github.com>
2020-08-20 21:59:45 +0900

    facilityのviewのリファクタリングと施設一覧へのデザイン適用 (#177)
    * facilityのviewのリファクタリングと施設一覧へのデザイン適用
    
    * 不要なルーティングの削除
    
    * scssの読み込みをimportに変更
    
    * 不要な記載の削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_07_31_133438) do
-
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "namespace"
