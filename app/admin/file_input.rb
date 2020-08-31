b1ee6dccaccca766d91da30350a7109068036c55
great084 <great084@gmail.com>
2020-09-01 00:49:16 +0900

    管理画面からmenu情報の登録を実装

---
+++ app/admin/file_input.rb
@@ -0,0 +1,23 @@
+class FileInput < Formtastic::Inputs::FileInput
+  def to_html
+    input_wrapping do
+      label_html <<
+      builder.file_field(method, input_html_options) <<
+      image_preview_content
+    end
+  end
+ 
+  private
+ 
+  def image_preview_content
+    image_preview? ? image_preview_html : ""
+  end
+ 
+  def image_preview?
+    options[:image_preview] && @object.send(method).present?
+  end
+ 
+  def image_preview_html
+    template.link_to(template.image_tag(@object.send(method).url, class: "image-preview", width: 200), @object.send(method).url, target: '_blank')
+  end
+end
--- app/admin/menus.rb
+++ app/admin/menus.rb
@@ -1,10 +1,12 @@
 ActiveAdmin.register Menu do
   belongs_to :facility
+  permit_params :name, :price, :content, :image, :created_user, :updated_user
 
   index do
     selectable_column
     column :image do |menu|
-      link_to image_tag("/menu_image/"+menu.image , class: 'image-thumbnail'), admin_facility_menu_path(menu.facility_id, menu.id)
+      # link_to image_tag("/menu_image/"+menu.image , class: 'image-thumbnail'), admin_facility_menu_path(menu.facility_id, menu.id)
+      link_to image_tag(menu.image.url, class: 'image-thumbnail'), admin_facility_menu_path(menu.facility_id, menu.id) if menu.image.url
     end
     id_column
     column :name
@@ -26,7 +28,8 @@ ActiveAdmin.register Menu do
       row :id
       row :name
       row :image do |menu|
-        image_tag "/menu_image/"+menu.image, class: 'image-thumbnail'
+        # image_tag "/menu_image/"+menu.image, class: 'image-thumbnail'
+        image_tag menu.image.url, class: 'image-thumbnail' if menu.image.url
       end
       row :price
       row :content
@@ -35,5 +38,20 @@ ActiveAdmin.register Menu do
       row :created_at
       row :updated_at
     end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :price
+      f.input :content
+      f.input :created_user
+      f.input :updated_user
+      # f.file_field :image, accept: "image/jpg,image/gif,image/jpeg,image/png"
+      # f.input :image, as: :file
+      # f.input :image, as: :file, :image => proc { |o| o.image.url(:thumb) }
+      f.input :image, :image_preview => true
+    end
+    f.actions
   end
 end
