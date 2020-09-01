c94e2a4f527debcdb1a7ef7812a8a19154331122
great084 <great084@gmail.com>
2020-09-01 15:45:35 +0900

    menu画像保存先をpublic/uplodad配下へ変更

--- app/uploaders/default_uploader.rb
+++ app/uploaders/default_uploader.rb
@@ -10,8 +10,8 @@ class DefaultUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "menu_image"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # "menu_image"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
