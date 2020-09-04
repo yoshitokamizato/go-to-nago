a938397e51ced07e8f318f35ee21149000798238
great084 <great084@gmail.com>
2020-09-04 16:39:32 +0900

    Facility_imageをcarrierwaveで登録参照する方式に変更＆一覧にはサムネイルを表示

--- app/uploaders/default_uploader.rb
+++ app/uploaders/default_uploader.rb
@@ -31,7 +31,7 @@ class DefaultUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [200, 150]
+    process resize_to_fill: [400, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
