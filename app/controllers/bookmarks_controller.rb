d5b42f0195099b3d52153a8911f3271b7a7d5f7b
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-09-10 03:31:51 +0900

    トップのいいねボタンのAjaxを変更（他のいいねボタンと仕様が違うため）

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,24 @@
 class BookmarksController < ApplicationController
-  before_action :set_facility, only: %i(create destroy)
+  before_action :set_facility, only: %i[create destroy]
   def create
     bookmark = current_user.bookmarks.build(facility_id: params[:facility_id])
     bookmark.save!
+
+    # 詳細画面の1つ目の言い値ボタンは他のと仕様が違うため
+    # flagで識別して、同じajaxの中でも別のコマンドが必要
+    return unless request.xhr?
+
+    @flag = params["flag"] unless params["flag"].blank?
   end
 
   def destroy
     current_user.bookmarks.find_by(facility_id: params[:facility_id]).destroy!
+
+    # 詳細画面の1つ目の言い値ボタンは他のと仕様が違うため
+    # flagで識別して、同じajaxの中でも別のコマンドが必要
+    return unless request.xhr?
+
+    @flag = params["flag"] unless params["flag"].blank?
   end
 
   private
