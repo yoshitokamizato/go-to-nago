47922fb4d3df522fdc7100a0fba18d5895ac3a35
ひろみち <36526480+great084@users.noreply.github.com>
2020-09-26 07:53:35 +0900

    sendgridでhtmlメールの有効化 (#242)

--- lib/mail/send_grid.rb
+++ lib/mail/send_grid.rb
@@ -7,11 +7,14 @@ class Mail::SendGrid
     from = SendGrid::Email.new(email: mail.from.first)
     to = SendGrid::Email.new(email: mail.to.first)
     subject = mail.subject
-    content = SendGrid::Content.new(type: 'text/plain', value: mail.body.raw_source)
+    content = SendGrid::Content.new(type: 'text/html', value: mail.body.raw_source)
     sg_mail = SendGrid::Mail.new(from, subject, to, content)
 
     sg = SendGrid::API.new(api_key: @settings[:api_key])
     response = sg.client.mail._('send').post(request_body: sg_mail.to_json)
-    puts response.status_code
+    puts "status_code=#{response.status_code}"
+    # puts "body=#{response.body}"
+    # puts "parse_body=#{response.parsed_body}"
+    # puts "headers=#{response.headers}"
   end
 end
