6a197641ccdf794ed98ca0d6ec8398d59e7df235
ひろみち <36526480+great084@users.noreply.github.com>
2020-10-30 15:29:56 +0900

    もっと見るボタンのエラー（スポットページでグルメが表示される）の修正 (#267)

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   get "/tos", to: "fixedpages#tos"
   get "/gourmet/:id", to: "facilities#show", as: "gourmet"
   get "/spot/:id", to: "facilities#show", as: "spot"
-  get "/gourmets/list", to: "facilities#index"
-  get "/spots/list", to: "facilities#index"
+  get "/gourmets/list", to: "facilities#gourmets_index"
+  get "/spots/list", to: "facilities#spots_index"
   get "/gourmet/:facility_id/menus", to: "menus#index", as: "gourmet_menus"
   get "/spot/:facility_id/menus", to: "menus#index", as: "spot_menus"
 
