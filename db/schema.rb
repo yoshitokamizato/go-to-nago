e791f74ab1449cd7e662db1226744b274ba15a29
great084 <great084@gmail.com>
2021-01-15 17:43:22 +0900

    不要な変更の戻し

--- db/schema.rb
+++ db/schema.rb
@@ -133,7 +133,7 @@ ActiveRecord::Schema.define(version: 2020_12_16_111346) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email", default: ""
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
